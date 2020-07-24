7fab83e4fd5e2c8321ca90dba1470349e1da1cd1
oibeas <oscaribeas@gmail.com>
2020-07-24 22:20:15 +0200

    Modificado el css, creados los eventos y la funcion pintar a medias.

---
+++ js/eventos.js
@@ -0,0 +1,8 @@
+//capturar los elementos con los que voy a inteactuar.
+let inputTarea = document.getElementById('textoTarea');
+let selecPrioridad = document.getElementById('prioridad');
+let btnGuardar = document.getElementById('botonGuardar');
+
+let filtroPrioridad = document.getElementById('buscar');
+let filtroTarea = document.getElementById('busquedaTarea');
+
--- js/funciones.js
+++ js/funciones.js
@@ -1,39 +1,40 @@
 let seccionTareas = document.querySelector('#tareas');
 
-
 function pintarTareas(pTareas) {
-
-    //crear los elementos primero
-    let article = document.createElement('article');
-    let h3 = document.createElement('h3');
-    let ul = document.createElement('ul');
-    let liEdad = document.createElement('li');
-    let liSS = document.createElement('li');
-    let div = document.createElement('div');
-    //creo la clase para el div
-    div.className = "diagnostico";
-
-    //creamos los contenidos
-    let contentH3 = document.createTextNode(pTareas[0].titulo + " " + pTareas[0].prioridad);
-    let contentliEdad = document.createTextNode('EDAD: ' + pTareas.edad);
-    let contentliSS = document.createTextNode('NUMERO SEGURIDAD SOCIAL: ' + pTareas.numeroSS);
-    let contentDiagnostico = document.createTextNode('Diagnostico: ' + pTareas.diagnostico);
-
-    //tiro los contenidos dentro de los elementos
-    liSS.appendChild(contentliSS);
-    liEdad.appendChild(contentliEdad);
-    h3.appendChild(contentH3);
-    div.appendChild(contentDiagnostico);
-
-    //Tirar dentro del article los diferentes elementos en orden
-    ul.appendChild(liEdad);
-    ul.appendChild(liSS);
-
-    article.appendChild(h3);
-    article.appendChild(ul);
-    article.appendChild(div);
-
-    seccionTareas.appendChild(article);
+    seccionTareas.innerHTML = "";
+    pTareas.forEach(element => {
+        seccionTareas.innerHTML += `<div>
+                                    <ul>
+                                        <li>${pTareas.titulo}</li>
+                                        <li>${pTareas.prioridad}</li>
+                                    </ul>
+                                </div>`;
+    });
 
 }
+
+
 pintarTareas(listaTareas);
+
+// function pintarNoticias(pListaNoticias) {
+//     seccionNoticias.innerHTML = "";
+
+//     pListaNoticias.forEach(noticia => {
+//         pintarNoticia(noticia);
+//     });
+
+// }
+
+// function borrarNoticia(pNoticiaBorrar) {
+
+//     let noticiaBorrar = document.getElementById(pNoticiaBorrar);
+//     seccionNoticias.removeChild(noticiaBorrar);
+//         let id = pNoticiaBorrar.substring(8)
+//     console.log(id);
+//     borrar(id)
+// }
+
+
+
+
+// pintarNoticias(blog);
